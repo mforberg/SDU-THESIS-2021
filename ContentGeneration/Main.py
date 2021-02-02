2a2f29c5f5b75e6ccd7194ba8c262708fd2034dd
SmiteyJonawar <jonasr16@student.sdu.dk>
2021-02-02 11:14:15 +0100

    Update Main.py

--- ContentGeneration/Main.py
+++ ContentGeneration/Main.py
@@ -1,5 +1,5 @@
 import grpc
-
+import time
 import minecraft_pb2_grpc
 from minecraft_pb2 import *
 
@@ -8,43 +8,56 @@ channel = grpc.insecure_channel('localhost:5001', options=options)
 client = minecraft_pb2_grpc.MinecraftServiceStub(channel)
 
 
-skip_list = [AIR, TALLGRASS, RED_FLOWER, YELLOW_FLOWER, LEAVES, VINE, SAPLING]  # List of blocks that is ignored
+skip_list = [AIR, TALLGRASS, RED_FLOWER, YELLOW_FLOWER, CHORUS_FLOWER, LEAVES, LEAVES2, VINE, SAPLING, WATERLILY,
+             RED_MUSHROOM, BROWN_MUSHROOM]  # List of blocks that is ignored
 
 
-def read_part_of_word(min_x, max_x, min_z, max_z):
+def read_part_of_world(min_x, max_x, min_z, max_z):
     result = client.readCube(Cube(
         min=Point(x=min_x, y=30, z=min_z),
         max=Point(x=max_x, y=200, z=max_z)
     ))
 
-    surface_dict = {}  # x z -> type y
-    block_dict = {}  # x y z -> type y
+    surface_dict = {}  # x z -> type y block
+    block_dict = {}  # x y z -> type
 
     # changes the class structure to dictionaries + finds the surface
     for block in result.blocks:
         if block.type not in skip_list:
             x, y, z = block.position.x, block.position.y, block.position.z
-            block_dict[x, y, z] = block.type
+            block_dict[x, y, z] = {"type": block.type}
             if (x, z) not in surface_dict:
-                surface_dict[x, z] = y, block.type
-            elif surface_dict[x, z][0] < y:
-                surface_dict[x, z] = y, block.type
+                surface_dict[x, z] = {"y": y, "type": block.type, "block": block}
+            elif surface_dict[x, z]["y"] < y:
+                surface_dict[x, z] = {"y": y, "type": block.type, "block": block}
     return surface_dict, block_dict
 
 
 class Main:
-    total_surface_dict = {}  # x z -> type y
-    total_block_dict = {}  # x y z -> type y
+    total_surface_dict = {}  # x z -> type y block
+    total_block_dict = {}  # x y z -> type
 
     def run(self):
-        for i in range(1, 401, 40):
-            sur_dict, block_dict = read_part_of_word(i, i+40, i, i+40)
-            self.total_surface_dict.update(sur_dict)
-            self.total_block_dict.update(block_dict)
-        print(len(self.total_block_dict))
+        # for i in range(1, 201, 40):
+        sur_dict, block_dict = read_part_of_world(1, 50, 1, 50)
+        self.total_surface_dict.update(sur_dict)
+        self.total_block_dict.update(block_dict)
+        print(self.total_surface_dict)
+
+    def build_surface(self):
+        blockies = []
+        for value in self.total_surface_dict.values():
+            block = value["block"]
+            block.type = STAINED_GLASS
+            blockies.append(block)
+        client.spawnBlocks(Blocks(blocks=blockies))
 
 
+first_time = time.time()
 Main().run()
+print(time.time() - first_time)
+Main().build_surface()
+
 
 # client.fillCube(FillCubeRequest(  # Clear a 20x10x20 working area
 #     cube=Cube(
