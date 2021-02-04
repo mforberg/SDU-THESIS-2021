e18260f750f3d0aa6df1b2fccf6508283f16f9d4
nicolai449 <nicolai@matthiesn4.dk>
2021-02-04 10:41:24 +0100

    Added unit instantiating of main

---
+++ tests/unit_tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestReadWorld(unittest.TestCase):
+    def test_buildsurface(self):
+        pass
+    
+
+    def test_readsurface(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
