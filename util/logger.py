94a579cf188ff7311f7e9b11ab5df21c797c7197
nicolai449 <nicolai@matthiesn4.dk>
2021-02-04 09:31:48 +0100

    loggers begun to be created

---
+++ util/logger.py
@@ -0,0 +1,27 @@
+import time
+import functools
+
+
+class Logger:
+
+    def time_logger(self, func):
+        @functools.wraps(func)
+        def time_wrapper(*args, **kwargs):
+            t_time = time.perf_counter()
+            value = func(*args, **kwargs)
+            e_time = time.perf_counter()
+            elapsed_time = e_time - t_time 
+            print(f"{func.__name__!r} ran in {elapsed_time:.4f} secs")
+            return value
+        return time_wrapper
+
+    def info_logger(self, func):
+        @functools.wraps(func)
+        def info_wrapper(*args, **kwargs):
+            t_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
+            c_time = time.ctime(end_time)
+            print(f"{c_time}: {result}")
+            return result
+        return info_wrapper
