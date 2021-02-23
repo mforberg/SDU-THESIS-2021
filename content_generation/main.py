e7aa0aabd73b159fe8e8b0f330d606fcd0220c7b
SmiteyJonawar <jonasr16@student.sdu.dk>
2021-02-23 11:39:52 +0100

    rollback implemented

--- content_generation/main.py
+++ content_generation/main.py
@@ -4,6 +4,7 @@ from map_ga.map_main import AreasGA
 import minecraft_pb2_grpc
 import grpc
 import time
+import copy
 
 
 class Main:
@@ -12,7 +13,9 @@ class Main:
         first_time = time.time()
         total_block_dict, total_surface_dict, district_areas = map_analysis.MapAnalysis().run()
         print(f"Map analysis - Time: {time.time() - first_time}")
-        self.reset_surface(surface_dict=total_surface_dict)
+        # reset = input("reset surface? y/N - type anything and it will reset, aka. put grass :))")
+        # if reset:
+        #     self.grass_surface(surface_dict=total_surface_dict)
         first_time = time.time()
         result = AreasGA().run(total_block_dict=total_block_dict, total_surface_dict=total_surface_dict,
                                areas=district_areas)
@@ -20,6 +23,9 @@ class Main:
         print(len(result['population']))
         for area in result['population']:
             self.build_surface(surface_dict=total_surface_dict, list_of_x_z_coordinates=area['area'])
+        rollback = input("reset surface? Y/n - type anything and it will not rollback")
+        if not rollback:
+            self.rollback(surface_dict=total_surface_dict)
 
     def build_surface(self, surface_dict, list_of_x_z_coordinates):
         options = [('grpc.max_send_message_length', 512 * 1024 * 1024),
@@ -29,19 +35,30 @@ class Main:
 
         blocks = []
         for value in list_of_x_z_coordinates:
-            block = surface_dict[value]['block']
+            block = copy.deepcopy(surface_dict[value]['block'])
             block.type = DIAMOND_ORE
             blocks.append(block)
         client.spawnBlocks(Blocks(blocks=blocks))
 
-    def reset_surface(self, surface_dict: dict):
+    def rollback(self, surface_dict):
         options = [('grpc.max_send_message_length', 512 * 1024 * 1024),
                    ('grpc.max_receive_message_length', 512 * 1024 * 1024)]
         channel = grpc.insecure_channel('localhost:5001', options=options)
         client = minecraft_pb2_grpc.MinecraftServiceStub(channel)
         blocks = []
         for value in surface_dict.values():
-            block = value['block']
+            block = copy.deepcopy(value['block'])
+            blocks.append(block)
+        client.spawnBlocks(Blocks(blocks=blocks))
+
+    def grass_surface(self, surface_dict: dict):
+        options = [('grpc.max_send_message_length', 512 * 1024 * 1024),
+                   ('grpc.max_receive_message_length', 512 * 1024 * 1024)]
+        channel = grpc.insecure_channel('localhost:5001', options=options)
+        client = minecraft_pb2_grpc.MinecraftServiceStub(channel)
+        blocks = []
+        for value in surface_dict.values():
+            block = copy.deepcopy(value['block'])
             if block.type in FLUID_LIST:
                 continue
             block.type = GRASS
