37482029723cd221792b262d1652a010b2540154
nicolai449 <nicolai@matthiesen4.dk>
2021-03-10 13:35:52 +0100

    Improved CLI

---
+++ content_generation/block_file_loader.py
@@ -0,0 +1,88 @@
+from map_variables import *
+import os.path
+import pickle
+import map_analysis
+
+class BlockFileLoader:
+    def __init__(self):
+        self.total_block_dict, self.total_surface_dict, self.district_areas, self.set_of_fluids = None, None, None, None
+
+    def run(self):
+        if os.path.exists(f'{save_file_dir}{save_file_path}'):
+            print('Do you want to run on save file? 1 or 2')
+            keyOne = self.fetch_user_integer()
+            #Should the user want to use a saved file
+            if keyOne == 1:
+                print('What save file do you want to use?')
+                print('Use the number corresponding to file')
+                block_file = self.file_selector(save_file_dir)
+                unpickled_block_file = pickle.load(block_file)
+                self.load_from_pkl_file(unpickled_block_file)
+            #If the user doesn't want to use a save file
+            else:
+                print('Do you want to save the old file before creating a new one? 1 or 2')
+                keyTwo = self.fetch_user_integer()
+                if keyTwo == 1:
+                    print("Give the file a name! Please don't use space")
+                    file_name = str(input())
+                    self.total_block_dict, self.total_surface_dict, self.district_areas, self.set_of_fluids = map_analysis.MapAnalysis().run()
+                    self.write_to_pkl_file(self.district_areas, self.set_of_fluids, self.total_block_dict, self.total_surface_dict, file_name)
+                elif keyTwo == 2:
+                    self.total_block_dict, self.total_surface_dict, self.district_areas, self.set_of_fluids = map_analysis.MapAnalysis().run()
+                    self.write_to_pkl_file(self.district_areas, self.set_of_fluids, self.total_block_dict, self.total_surface_dict)
+        else:
+            self.total_block_dict, self.total_surface_dict, self.district_areas, self.set_of_fluids = map_analysis.MapAnalysis().run()
+            self.write_to_pkl_file(self.district_areas, self.set_of_fluids, self.total_block_dict, self.total_surface_dict)
+
+    def fetch_user_integer(self):
+        keyTwo = None
+        while True:
+            try:
+                keyTwo = int(input())
+                break
+            except:
+                print("Please put in a integer")
+        return keyTwo
+
+    def file_selector(self, dir_save_file):
+        dirs = os.listdir(dir_save_file)
+        for index, file in enumerate(dirs):
+            if file.endswith('.pkl'):
+                print(f'{index+1}: {file}')
+        stopper = self.fetch_user_integer()
+        if stopper <= 0:
+            stopper = stopper + 1
+        block_file = open(f'{save_file_dir}{dirs[stopper-1]}', 'rb')
+        return block_file
+
+    def load_from_pkl_file(self, unpickled_block_file):
+        if self.check_range(unpickled_block_file):
+            self.total_block_dict, self.total_surface_dict, self.district_areas, self.set_of_fluids = \
+                unpickled_block_file['total_block_dict'], unpickled_block_file['total_surface_dict'], \
+                unpickled_block_file['district_areas'], \
+                unpickled_block_file['set_of_fluids']
+
+
+    def check_range(self, file):
+        x_range = file['x_range']
+        z_range = file['z_range']
+
+        if x_range[0] == BOX_X_MIN and x_range[1] == BOX_X_MAX and z_range[0] == BOX_Z_MIN and z_range[1] == BOX_Z_MAX:
+            return True
+        else:
+            return False
+
+    def write_to_pkl_file(self, district_areas, set_of_fluids, total_block_dict, total_surface_dict, file_name = 'f'):
+        data = {}
+        data['x_range'] = [BOX_X_MIN, BOX_X_MAX]
+        data['z_range'] = [BOX_Z_MIN, BOX_Z_MAX]
+        data['total_block_dict'] = total_block_dict
+        data['total_surface_dict'] = total_surface_dict
+        data['district_areas'] = district_areas
+        data['set_of_fluids'] = set_of_fluids
+        if file_name == 'f':
+            with open(f'{save_file_dir}{save_file_path}', 'wb') as output:
+                pickle.dump(data, output)
+        else:
+            with open(f'{save_file_dir}{file_name}_{save_file_path}', 'wb') as output:
+                pickle.dump(data, output)
