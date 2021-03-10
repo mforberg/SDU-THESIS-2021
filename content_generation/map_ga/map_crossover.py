52c27e3839b4549f273ec31ef60f866525f1aa6d
SmiteyJonawar <jonasr16@student.sdu.dk>
2021-03-10 12:18:04 +0100

    multiple crossover options

--- content_generation/map_ga/map_crossover.py
+++ content_generation/map_ga/map_crossover.py
@@ -39,19 +39,40 @@ class MapCrossover:
         random.shuffle(parent1)
         random.shuffle(parent2)
         if len(parent1) > len(parent2):
-            return self.__single_point_crossover(shortest=parent2, longest=parent1)
+            return self.__k_point_crossover(k=1, shortest=parent2, longest=parent1)
         else:
-            return self.__single_point_crossover(shortest=parent1, longest=parent2)
+            return self.__k_point_crossover(k=1, shortest=parent1, longest=parent2)
 
-    def __single_point_crossover(self, shortest: List[SolutionArea], longest: List[SolutionArea]) -> dict:
-        point = random.randint(0, len(shortest)-1)
+    def __k_point_crossover(self, k: int, shortest: List[SolutionArea], longest: List[SolutionArea]) -> dict:
+        points = set()
+        while len(shortest) > len(points) and k >= len(points):
+            new_value = False
+            while not new_value:
+                point = random.randint(0, len(shortest)-1)
+                if point not in points:
+                    new_value = True
+                    points.add(point)
         child1 = []
         child2 = []
         flip = False
         for i in range(0, len(longest)):
-            if i == point:
-                flip = True
+            if i in points:
+                flip = not flip
             if not flip:
+                if i < len(shortest):
+                    child1.append(shortest[i])
+                child2.append(longest[i])
+            else:
+                if i < len(shortest):
+                    child2.append(shortest[i])
+                child1.append(longest[i])
+        return {"c1": child1, "c2": child2}
+
+    def __uniform_crossover(self, shortest: List[SolutionArea], longest: List[SolutionArea]) -> dict:
+        child1 = []
+        child2 = []
+        for i in range(0, len(longest)):
+            if random.randint(0, 1) == 0:
                 if i < len(shortest):
                     child1.append(shortest[i])
                 child2.append(longest[i])
