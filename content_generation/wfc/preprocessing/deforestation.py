bf1e3159ec026c34f3527a2cbc37d97da5df8397
SmiteyJonawar <jonasr16@student.sdu.dk>
2021-04-07 11:34:33 +0200

    :wave:

---
+++ content_generation/wfc/preprocessing/deforestation.py
@@ -0,0 +1,18 @@
+
+
+class Deforest:
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def __find_trees(self):
+        pass
+
+    def __read_tree(self):
+        pass
+
+    def __remove_trees(self):
+        pass
