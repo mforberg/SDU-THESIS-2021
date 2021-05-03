cc0cf0dd531485ba2ce4952df63b87f99f8c82cb
SmiteyJonawar <jonasr16@student.sdu.dk>
2021-05-03 14:21:41 +0200

    yahoo, no prints

--- content_generation/map_ga/map_main.py
+++ content_generation/map_ga/map_main.py
@@ -49,8 +49,8 @@ class AreasGA:
                 #  set population and clean it
                 population = crossed_population_no_fitness
                 self.__clean_population_for_duplicates(population=population)
-        for string in self.new_best_prints:
-            print(string)
+        # for string in self.new_best_prints:
+        #     print(string)
         return self.best_solution
 
     def __clean_population_for_duplicates(self, population: List[SolutionGA]):
